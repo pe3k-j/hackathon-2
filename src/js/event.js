d89178383515270ab4c4f17d6c12879174fcffe2
Joe <joetkane@gmail.com>
2023-09-29 10:59:37 +0100

    working fetch and load data function

--- src/js/event.js
+++ src/js/event.js
@@ -1,4 +1,4 @@
-
+const mainContainer = document.querySelector('main')
 
  export class EventWidget {
     constructor(name, id,  date, description, img) {
@@ -17,7 +17,7 @@
         <div class="event__title">${this.title}</div>
         <div class="event__date">Whenn? ${this.date}</div>
         <div class="event__description">${this.description}</div>
-        div class="event__btn">Find out more</div>              
+        <div class="event__btn"><button>Find out more</button></div>              
         <div class="event__img" ><img src="${this.img}" alt="${this.title}"></div>
         </div>`                                                 //add in extension the form element for submission & change button & eventlistener
         ;
@@ -29,7 +29,7 @@
     //      findOutMore.addEventListener('click', ()=> {               //change eventlistner to post data from form
     //     modalWindow.className = "modal__display"
     //   })
-
+        mainContainer.appendChild(this.element)
     } // end of refresh element
 
     //querySelector for subsequent fucntionality
