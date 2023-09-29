f2fc6e0b189fd0f9b1955b9187cbadc2910d6fe2
Joe <joetkane@gmail.com>
2023-09-29 10:13:57 +0100

    additional comments

--- src/js/event.js
+++ src/js/event.js
@@ -1,6 +1,8 @@
+
+
 class EventWidget {
     constructor(name, date, description, img) {
-        this.element = document.createElement('div')
+        this.element = document.createElement('div')        //change on extension parent.cre.....
         this.name = name
         this.date = date
         this.description = description
@@ -14,17 +16,18 @@ class EventWidget {
         <div class="event__title">${this.title}</div>
         <div class="event__date">Whenn? ${this.date}</div>
         <div class="event__description">${this.description}</div>
-        div class="event__btn">Find out more</div>
+        div class="event__btn">Find out more</div>              
         <div class="event__img" ><img src="${this.img}" alt="${this.title}"></div>
-        </div>`;
+        </div>`                                                 //add in extension the form element for submission & change button & eventlistener
+        ;
         //querySelector for subsequent fucntionality
          const findOutMore = this.element.querySelector(".event__btn")
          const modalWindow = document.querySelector(".modal")
         
-         //event listner for modal window to pop up
-        //  findOutMore.addEventListener('click', ()=> {
-        //     modalWindow.className = "modal__display"
-        //  })
+        // event listner for modal window to pop up
+         findOutMore.addEventListener('click', ()=> {               //change eventlistner to post data from form
+        modalWindow.className = "modal__display"
+      })
 
     } // end of refresh element
 
